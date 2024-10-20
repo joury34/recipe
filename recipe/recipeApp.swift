57f56c01fa3cb9b2f877e5543ade620c5b800323
joury34 <joury34@outlook.com>
2024-10-20 10:04:52 +0300

    Initial Commit

---
+++ recipe/recipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipeApp.swift
+//  recipe
+//
+//  Created by Joury on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct recipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ recipeTests/recipeTests.swift
@@ -0,0 +1,17 @@
+//
+//  recipeTests.swift
+//  recipeTests
+//
+//  Created by Joury on 17/04/1446 AH.
+//
+
+import Testing
+@testable import recipe
+
+struct recipeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
